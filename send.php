0f4565a398f0e62832a910bf9dc90acbcd598e0e
Steeve Droz <steeve.droz@rpn.ch>
2017-06-13 08:48:07 +0200

    Utilisation de flexbox

--- send.php
+++ send.php
@@ -3,7 +3,6 @@
 if (!empty($_POST['message']) && !empty($_POST['nickname']))
 {
     file_put_contents('chat.txt', '(' . date('H:i:s') . ')[' . trim($_POST['nickname']) . ']' . trim($_POST['message']) . "\n", FILE_APPEND);
-    echo 'OK';
 }
 
 ?>
